cfbf6334590867c6a4f97fa77f1cadb9ba150395
david porosnicu <david94_p@yahoo.com>
2018-11-26 18:13:47 +0000

    add mongolab

--- app.js
+++ app.js
@@ -19,7 +19,9 @@ var commentRoutes    = require("./routes/comments"),
     indexRoutes      = require("./routes/index");
 
     
-mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
+mongoose.connect("mongodb://david:yelpcampv2@ds117334.mlab.com:17334/yelpcamp_v2");
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
